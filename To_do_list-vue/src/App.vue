42cc6a674c174d96d2d28e615cbfd0ee1c720675
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2022-08-02 12:56:20 -0300

    Lista de tarefas com Vue

---
+++ To_do_list-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<Form/>
+
+</template>
+
+<script>
+import Form from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Form
+  }
+}
+</script>
+
+<style >
+
+</style>
---
+++ To_do_list-vue/src/components/Form.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="container">
+  <h1>To Do list </h1>
+  <input v-model="campo" type="text" placeholder="Tarefa">
+  <button @click="adicionar" type="button">Adicionar</button>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{task}}</li>
+    
+  </ul>
+</div>
+
+</template>
+
+<script>
+export default {
+  name:'Form',
+data(){
+return{
+  campo:'',
+  tasks:[]
+}
+},
+methods:{
+adicionar(){
+this.tasks.push(this.campo)
+this.campo=''
+ }
+ 
+}
+}
+
+</script>
+
+<style scoped>
+
+.container{
+  display: flex;
+  justify-content: center; 
+  align-items: center;
+  flex-direction: column;
+  
+}
+h1{
+  font-family:  sans-serif;
+}
+button{
+  margin-top: 0.5rem;
+  font-size: 1em;
+  background: rgb(44, 43, 43);
+  color: white;
+  border-radius: 5px;
+  cursor: pointer;
+}
+li{
+  position: relative;
+  right: 1rem;
+}
+</style>
