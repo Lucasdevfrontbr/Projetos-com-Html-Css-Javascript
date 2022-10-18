c8a01949a97324a89f536b0267e1e4b47bc0c344
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2022-10-18 11:40:47 -0300

    Cep-APP

---
+++ Appcep_React/Cep/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [value, setvalue] = useState('')
+  const [cep, setcep] = useState({})
+  function GetCep(){
+if(value===''){
+alert('digite um valor valido')
+return
+  }
+  fetch(`https://viacep.com.br/ws/${value}/json/`)
+  .then(function(response){
+    return response.json()
+
+  })
+ .then(res=>{
+setcep(res)
+  })
+}
+
+  return (
+    <div className="App">
+     
+      <main className='main'>
+      <h1>Buscador de Cep</h1>
+      <div className='form'>
+<input  type="text" placeholder='digite um Cep' value={value} onChange={(e)=>setvalue(e.target.value)} />
+
+<button className='btn' onClick={GetCep}>Buscar</button>
+      
+      </div>
+        <section className='results' >
+        <span className='item'>{cep.logradouro}</span> 
+        <span className='item'>{cep.complemento}</span> 
+        <span className='item'>{cep.bairro}</span> 
+        <span className='item'>{cep.localidade} {cep.uf}</span>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default App
