3fd66336acaefb248cc96b808a49c8bae05ff4d6
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2022-10-20 12:05:18 -0300

    Update App.jsx

--- Appcep_React/Cep/src/App.jsx
+++ Appcep_React/Cep/src/App.jsx
@@ -17,7 +17,8 @@ return
   })
  .then(res=>{
 setcep(res)
-  })
+  }).catch(alert('Ocorreu um erro'))
+setvalue("")
 }
 
   return (
@@ -29,17 +30,20 @@ setcep(res)
 <input  type="text" placeholder='digite um Cep' value={value} onChange={(e)=>setvalue(e.target.value)} />
 
 <button className='btn' onClick={GetCep}>Buscar</button>
+    
+</div>
+{value &&(
+<section className='results' >
+<span className='item'>{cep.logradouro}</span> 
+<span className='item'>{cep.complemento}</span> 
+<span className='item'>{cep.bairro}</span> 
+<span className='item'>{cep.localidade} {cep.uf}</span>
+</section>)}
+</main>
+</div>
+)
       
-      </div>
-        <section className='results' >
-        <span className='item'>{cep.logradouro}</span> 
-        <span className='item'>{cep.complemento}</span> 
-        <span className='item'>{cep.bairro}</span> 
-        <span className='item'>{cep.localidade} {cep.uf}</span>
-        </section>
-      </main>
-    </div>
-  )
+  
 }
 
 export default App
