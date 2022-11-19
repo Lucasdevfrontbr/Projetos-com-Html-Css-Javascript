2532b91cf9b3e5a9c2c4b47f811f8b5a867a757e
Lucasdevfrontbr <Lucasfrontdev@gmail.com>
2022-11-19 20:25:01 -0300

    Update HeaderElement.jsx

--- Movies_React/Movie/src/components/HeaderElement.jsx
+++ Movies_React/Movie/src/components/HeaderElement.jsx
@@ -1,15 +1,12 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 export function HeaderElement(){
     return (<div>
      <header className='header'>
 <nav className='navbar'>
 <ul className='list'>
-<a className='item' href="/">
-  <li>Filmes Populares</li>
-</a>
-<a className='item' href="/Melhores">
-  <li>Melhores Qualificados</li>
-</a>
+ <Link className='item' to='/'>Filmes Populares</Link>
+ <Link className='item' to='/Melhores'>Melhores Qualificados</Link>
 </ul>
 
 
